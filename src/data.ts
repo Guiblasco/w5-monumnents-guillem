49299deee191f80f6ba3a4fa44f77bd447bee62c
Guillem Blasco <gowet92@gmail.com>
2024-08-06 19:53:25 +0200

    Create a monument type and an array monuments

---
+++ src/data.ts
@@ -0,0 +1,83 @@
+import type { Monument } from "./types";
+
+const worldMonuments: Monument[] = [
+  {
+    id: "1",
+    name: "Taj Mahal",
+    city: "Agra",
+    country: "India",
+    description: `El Taj Mahal (Mahal, 'Corona de los palacios' es un monumento
+     funerario construido entre 1632 y 1654 en la ciudad de Agra, estado de Uttar Pradesh (India),
+     a orillas del río Yamuna, por el emperador musulmán Shah Jahan de la dinastía mogol. 
+     El imponente conjunto de edificios se erigió en honor de su esposa favorita,
+     Arjumand Banu Begum —más conocida como Mumtaz Mahal—, que murió en el parto de su decimocuarto hijo.
+     Se estima que en su construcción trabajaron unos 20 000 obreros bajo dirección de un conjunto de arquitectos 
+     liderados por el arquitecto de la corte, Ustad Ahmad Lahori.`,
+    imageUrl:
+      "https://es.wikipedia.org/wiki/Taj_Mahal#/media/Archivo:Taj_Mahal_(Edited).jpeg",
+  },
+  {
+    id: "2",
+    name: "Torre Eiffel",
+    city: "Paris",
+    country: "Francia",
+    description: `La torre Eiffel (tour Eiffel, en francés),
+    inicialmente llamada Tour de 300 mètres («Torre de 300 metros»)
+    es una estructura de hierro pudelado diseñada inicialmente 
+    por los ingenieros civiles Maurice Koechlin y Émile Nouguier 
+    y construida, tras el rediseño estético de Stephen Sauvestre, 
+    por el ingeniero civil francés Gustave Eiffel y sus colaboradores
+    para la Exposición Universal de 1889 en París (Francia).`,
+    imageUrl:
+      "https://es.wikipedia.org/wiki/Torre_Eiffel#/media/Archivo:Eiffelturm.JPG",
+  },
+  {
+    id: "3",
+    name: "Estatua de la Libertad",
+    city: "New York",
+    country: "Estados Unidos",
+    description: `La Libertad iluminando el mundo, más conocida como la Estatua de la Libertad,  
+    es uno de los monumentos más famosos de Nueva York, de los Estados Unidos y de todo el mundo. 
+    Se encuentra en la isla de la Libertad al sur de la isla de Manhattan, 
+    junto a la desembocadura del río Hudson y cerca de la isla Ellis. 
+    La Estatua de la Libertad fue un regalo del pueblo francés al pueblo estadounidense en 1886 
+    para conmemorar el centenario de la Declaración de Independencia de los Estados Unidos y 
+    como un signo de amistad entre las dos naciones. 
+    Fue inaugurada el 28 de octubre de 1886 en presencia del presidente estadounidense 
+    de la época, Grover Cleveland. La estatua es obra del escultor francés Frédéric Auguste Bartholdi
+     y la estructura interna fue diseñada por el ingeniero Alexandre Gustave Eiffel. 
+     El arquitecto francés Eugène Viollet-le-Duc se encargó de la elección de los cobres utilizados para la construcción de la estatua. 
+     El 15 de octubre de 1924, la estatua fue declarada como monumento nacional de los Estados Unidos y 
+     el 15 de octubre de 1965 se añadió la isla Ellis. Desde 1984 es considerada Patrimonio de la Humanidad por la Unesco`,
+    imageUrl:
+      "https://es.wikipedia.org/wiki/Estatua_de_la_Libertad#/media/Archivo:Statue_of_Liberty_7.jpg",
+  },
+  {
+    id: "4",
+    name: "Ópera de Sídney",
+    city: "Sidney",
+    country: "Australia",
+    description: `La Ópera de Sídney, situada en la ciudad de Sídney, 
+    estado de Nueva Gales del Sur, Australia, es uno de los edificios más famosos
+     y distintivos del siglo XX. Declarado en 2007 Patrimonio de la Humanidad, 
+     fue diseñado por el arquitecto danés Jørn Utzon en 1957 e inaugurado el 20 de octubre de 1973. 
+     En el edificio se realizan obras de teatro, ballet, ópera o producciones musicales. 
+     Es sede de la compañía de Ópera de Australia, la Compañía de Teatro de Sídney y la Orquesta Sinfónica de Sídney. 
+     Está administrada por la Opera House Trust, un organismo público del gobierno estatal de Nueva Gales del Sur.`,
+    imageUrl:
+      "https://es.wikipedia.org/wiki/%C3%93pera_de_S%C3%ADdney#/media/Archivo:Sydney_Opera_House_-_Dec_2008.jpg",
+  },
+  {
+    id: "5",
+    name: "Coliseo",
+    city: "Roma",
+    country: "Italia",
+    description: `El Coliseo o Anfiteatro Flavio es un anfiteatro de la época del Imperio romano, 
+    construido en el siglo I. Está ubicado en el este del Foro Romano, 
+    y fue el más grande de los que se construyeron en el Imperio romano. 
+    Conocido originalmente como Anfiteatro Flavio pasa a ser llamado Coliseo porque a su lado había una gran estatua, 
+    el Coloso de Nerón, un monumento dedicado al emperador Nerón que posteriormente sufrió transformaciones y llegó a desaparecer.`,
+    imageUrl:
+      "https://es.wikipedia.org/wiki/Coliseo#/media/Archivo:Colosseo_2020.jpg",
+  },
+];
