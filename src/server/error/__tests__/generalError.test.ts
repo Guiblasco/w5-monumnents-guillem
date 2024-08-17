a0921cbfad775b5f9dff80391c1f56e1569b3825
Guillem Blasco <gowet92@gmail.com>
2024-08-17 15:05:08 +0200

    Add generalError test

---
+++ src/server/error/__tests__/generalError.test.ts
@@ -0,0 +1,32 @@
+import { json, type NextFunction, type Request, type Response } from "express";
+import ServerError from "../ServerError";
+import { generalError } from "../generalError";
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+describe("Given the funcion generalError", () => {
+  describe("When it receives a request", () => {
+    const expectStatusCode = 404;
+    const expectErrorMessage = { message: "Not found" };
+    const error = new ServerError("Not found", 404);
+    const req: Partial<Request> = {};
+    const res: Partial<Response> = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn().mockReturnThis(),
+    };
+    const next: NextFunction = jest.fn();
+
+    test("Then it should call a response's method status with error's status code 404", () => {
+      generalError(error, req as Request, res as Response, next);
+
+      expect(res.status).toHaveBeenCalledWith(expectStatusCode);
+    });
+    test("Then it should call a response's method json an object property message value 'Not found'", () => {
+      generalError(error, req as Request, res as Response, next);
+
+      expect(res.json).toHaveBeenCalledWith(expectErrorMessage);
+    });
+  });
+});
