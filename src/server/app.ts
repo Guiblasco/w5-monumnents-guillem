cf8009f876a53fd953f7ebabce63937d9bd86d50
Guillem Blasco <gowet92@gmail.com>
2024-08-18 23:36:01 +0200

    Delete generalError test folder duplicate and rename unknownEndpoint

--- src/server/app.ts
+++ src/server/app.ts
@@ -4,7 +4,7 @@ import monumentsRouter from "../monument/router/monumentsRouter.js";
 import cors from "cors";
 import serverRouter from "./router/serverRouter.js";
 import { generalError } from "./error/generalError/generalError.js";
-import { unknownPath } from "./unknownPath/unknownPath.js";
+import { unknownPath } from "./unknownEndpoint/unknownEndpoint.js";
 
 const app = express();
 
